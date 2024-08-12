15d0cfd3256c736680913396440da9ebe19f91e2
itevie <izzyplays07@gmail.com>
2024-08-12 20:34:24 +0100

    s

--- src/commands/badges/badgeList.ts
+++ src/commands/badges/badgeList.ts
@@ -1,5 +1,8 @@
+import { User } from "discord.js";
 import { HypnoCommand } from "../../types/command";
+import { getAllAquiredBadges } from "../../util/actions/badges";
 import badges, { formatBadges } from "../../util/badges";
+import { createEmbed } from "../../util/other";
 
 const command: HypnoCommand = {
     name: "badgelist",
@@ -7,10 +10,25 @@ const command: HypnoCommand = {
     description: "Get a list of badges",
     type: "badges",
 
-    handler: (message, args) => {
-        let badgeText = formatBadges(badges);
+    handler: async (message, args) => {
+        let aquiredBadges = await getAllAquiredBadges();
+        let text = "";
 
-        return message.reply(`Here are the badges:\n\n${badgeText.join("\n")}`)
+        for (let i in badges) {
+            let amount = aquiredBadges.filter(x => x.badge_name === i).length;
+            let theUser: User | null;
+            if (amount === 1)
+                theUser = await message.client.users.fetch(aquiredBadges.find(x => x.badge_name === i).user);
+            text += `${badges[i].emoji} \`${badges[i].name}\` *${badges[i].description}*\n - ${amount} ${amount === 1 ? "person has" : "people have"} this${theUser ? ` (${theUser.username})` : ""}\n\n`;
+        }
+
+        return message.reply({
+            embeds: [
+                createEmbed()
+                    .setTitle(`List of badges`)
+                    .setDescription(text)
+            ]
+        });
     }
 };
 
