c59548d719d112e0aa829401c4879a86272f59d6
itevie <izzyplays07@gmail.com>
2024-08-13 00:43:51 +0100

    asd

--- src/commands/count/currentCount.ts
+++ src/commands/count/currentCount.ts
@@ -3,8 +3,9 @@ import { getServerCount } from "../../util/actions/serverCount";
 
 const command: HypnoCommand = {
     name: "currentcount",
+    aliases: ["highestcount"],
     type: "counting",
-    description: "Get the expected next number in the count",
+    description: "Get the expected next number in the count and the highest number",
 
     handler: async (message) => {
         let count = await getServerCount(message.guild.id);
@@ -12,7 +13,7 @@ const command: HypnoCommand = {
             return message.reply(`This server does not have counting enabled... but what if it did?`);
         }
 
-        return message.reply(`The **next** expected number is **${count.current_count + 1}**`);
+        return message.reply(`The **next** expected number is **${count.current_count + 1}**\nThe **highest** count is: **${count.highest_count}`);
     }
 };
 
