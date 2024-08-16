ae560b7eb2f80fd504cf9a61be8116e3d4b66fda
itevie <izzyplays07@gmail.com>
2024-08-16 03:28:24 +0100

    vc time.

---
+++ src/commands/leaderboards/vcTime.ts
@@ -0,0 +1,23 @@
+import { HypnoCommand } from "../../types/command";
+import { getAllGuildsUserData } from "../../util/actions/userData";
+import createLeaderboardFromData from "../../util/createLeaderboard";
+
+const command: HypnoCommand = {
+    name: `vctime`,
+    description: `See who has yapped in VC the longest`,
+    type: "leaderboards",
+
+    handler: async (message) => {
+        let data = await getAllGuildsUserData(message.guild.id);
+        let organised = data.map(x => [x.user_id, x.vc_time]) as [string, number][];
+
+        return message.reply({
+            embeds: [
+                (await createLeaderboardFromData(organised, "Who has yapped the most in VC?"))
+                    .setTitle(`Most VC time!`)
+            ]
+        })
+    }
+};
+
+export default command;
