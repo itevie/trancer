f574dbc968035d3d5d08273f110e5697efce0f64
itevie <izzyplays07@gmail.com>
2024-08-31 19:25:45 +0100

    multiple card pulls

--- src/commands/cards/pull.ts
+++ src/commands/cards/pull.ts
@@ -4,8 +4,9 @@ import { addCardFor } from "../../util/actions/cards";
 import { getAquiredItem, getItem, removeItemFor } from "../../util/actions/items";
 import { generateCardEmbed } from "../../util/cards";
 import { database } from "../../util/database";
+import { createEmbed } from "../../util/other";
 
-const command: HypnoCommand<{ deckId: number }> = {
+const command: HypnoCommand<{ deckId: number, amount: number }> = {
     name: "pull",
     type: "cards",
     description: "Get a new card, check command `rarities` to see chances, requires the card-pull item",
@@ -16,6 +17,10 @@ const command: HypnoCommand<{ deckId: number }> = {
             {
                 name: "deckId",
                 type: "wholepositivenumber"
+            },
+            {
+                name: "amount",
+                type: "wholepositivenumber",
             }
         ]
     },
@@ -24,41 +29,62 @@ const command: HypnoCommand<{ deckId: number }> = {
         // Check if user has the pull item
         let item = await getAquiredItem(config.cards.pullItemID, message.author.id);
         let shopItem = await getItem(config.cards.pullItemID);
-        if (item.amount === 0)
-            return message.reply(`You do not have any **${shopItem.name}**s!`);
+        let amount = args.amount ? args.amount : 1;
+        if (amount > item.amount)
+            return message.reply(`You do not have **${amount} ${shopItem.name}**s!`);
 
-        // Get the rarity
-        let rarity: Rarity;
-        for (const i in config.cards.weights) {
-            // Check if random is LESS than the rarity
-            // Because 0.34234 < 0.05 = false
-            if (Math.random() < config.cards.weights[i]) {
-                rarity = i as Rarity;
-                break;
+        let getCard: () => Promise<Card | string> = async () => {
+            // Get the rarity
+            let rarity: Rarity;
+            for (const i in config.cards.weights) {
+                // Check if random is LESS than the rarity
+                // Because 0.34234 < 0.05 = false
+                if (Math.random() < config.cards.weights[i]) {
+                    rarity = i as Rarity;
+                    break;
+                }
             }
+
+            // Obviously, there might be no matches, so just pick one of these
+            if (!rarity) rarity = Math.random() < 0.6 ? "common" : "uncommon";
+
+            // Get the card
+            let cards = await database.all(`SELECT * FROM cards WHERE rarity = ? AND deck = ?`, rarity, args.deckId) as Card[];
+
+            // Check if there was any
+            if (cards.length === 0)
+                return `Oops! I chose a rarity which has no cards`;
+
+            // Get random card
+            let card = cards[Math.floor(Math.random() * cards.length)];
+            return card;
         }
 
-        // Obviously, there might be no matches, so just pick one of these
-        if (!rarity) rarity = Math.random() < 0.6 ? "common" : "uncommon";
+        let it = " ".repeat(amount).split("");
+        let cards: Card[] = [];
 
-        // Get the card
-        let cards = await database.all(`SELECT * FROM cards WHERE rarity = ? AND deck = ?`, rarity, args.deckId) as Card[];
+        for await (const _ of it) {
+            let card = await getCard();
+            if (typeof card === "string")
+                return message.reply(`Failed to fetch card rarity in that deck!`);
+            await addCardFor(message.author.id, card.id);
+            await removeItemFor(message.author.id, config.cards.pullItemID);
+            cards.push(card);
+        }
 
-        // Check if there was any
-        if (cards.length === 0)
-            return message.reply(`Oops! I chose a rarity which has no cards`);
+        if (cards.length === 1) {
+            return message.reply({
+                embeds: [
+                    await generateCardEmbed(cards[0])
+                ]
+            });
+        }
 
-        // Get random card
-        let card = cards[Math.floor(Math.random() * cards.length)];
-
-        // Add & remove stuff
-        await addCardFor(message.author.id, card.id);
-        await removeItemFor(message.author.id, config.cards.pullItemID);
-
-        // Done
         return message.reply({
             embeds: [
-                await generateCardEmbed(card)
+                createEmbed()
+                    .setTitle("Your cards!")
+                    .setDescription(`${cards.map(x => `**${x.name}** *${x.rarity}*`).join("\n")}`)
             ]
         });
     }
