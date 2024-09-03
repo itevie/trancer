e34514594ae3e5efc51db1725de202b6f7cf90be
itevie <izzyplays07@gmail.com>
2024-09-03 20:49:26 +0100

    botserver only

--- src/messageHandlers/economy.ts
+++ src/messageHandlers/economy.ts
@@ -11,6 +11,8 @@ const handler: HypnoMessageHandler = {
     noCommands: true,
 
     handler: async (message) => {
+        if (message.guild.id !== config.botServer.id) return;
+
         if (config.economy.messagePayout.limit - (Date.now() - (timeouts[message.author.id] ?? 0)) < 0) {
             let money = randomFromRange(config.economy.messagePayout.min, config.economy.messagePayout.max);
             await addMoneyFor(message.author.id, money, "messaging");
