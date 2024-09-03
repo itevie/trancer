ed683706d10c4e83d76e91eaebb25955d6914cd6
itevie <izzyplays07@gmail.com>
2024-09-03 20:49:59 +0100

    asd

--- src/messageHandlers/messagesSentAdder.ts
+++ src/messageHandlers/messagesSentAdder.ts
@@ -9,25 +9,9 @@ const handler: HypnoMessageHandler = {
     description: "Adds 1 to your total sent messages",
 
     handler: async (message) => {
+        if (message.guild.id !== config.botServer.id) return;
         if (config.modules.statistics.enabled && !config.modules.statistics.ignoreChannels.includes(message.channel.id)) {
             addMessageSent(message.author.id, message.guild.id);
-
-            /*// Get words
-            let words = message.content.toLowerCase().split(" ").map(x => x.replace(/[^0-9a-z]/g, ""));
-            let amounts: { [key: string]: number } = {};
-            for (const word of words) {
-                if (!amounts[word]) amounts[word] = 0;
-                amounts[word]++;
-            }
-
-            if (Object.keys(amounts).length !== 0) {
-                let queries: [ISqlite.SqlType, any[]][] = [];
-
-                for (const i in amounts) {
-                    queries.push(``)
-                }
-                await analyticDatabase.run()
-            }*/
         }
     }
 };
