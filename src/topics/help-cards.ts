792e16b9b2c64601afd440461d4fa91e818c20a6
itevie <izzyplays07@gmail.com>
2024-09-08 17:17:30 +0100

    asd

--- src/topics/help-cards.ts
+++ src/topics/help-cards.ts
@@ -2,7 +2,6 @@ import { MessageCreateOptions } from "discord.js";
 import { createEmbed } from "../util/other";
 
 let message: MessageCreateOptions = {
-    content: "Test",
     embeds: [
         createEmbed()
             .setTitle("Cards")
