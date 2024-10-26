82ec8dae5df26b83ddd5247c34b6c79082f78071
Isabella <izzyplays07@gmail.com>
2024-10-26 23:31:36 +0100

    f

--- src/commands/dawnagotchi/reviveDawn.ts
+++ src/commands/dawnagotchi/reviveDawn.ts
@@ -19,7 +19,7 @@ const command: HypnoCommand = {
     if (economy.balance < 2500)
       return message.reply(`You need **2500 ${config.economy.currency}** to revive your Dawn, you horrible person.`);
 
-    await database.run(`UPDATE dawnagotchi SET alive = true, next_feed = CURRENT_TIMESTAMP, next_drink = CURRENT_TIMESTAMP, next_play = CURRENT_TIMESTAMP WHERE id = ?;`, dawn.id);
+    await database.run(`UPDATE dawnagotchi SET alive = true, next_feed = ?, next_drink = ?, next_play = ? WHERE id = ?;`, Date.now(), Date.now(), Date.now(), dawn.id);
     await removeMoneyFor(message.author.id, 2500);
 
     return message.reply(`Your Dawn was revived for **2500 ${config.economy.currency}**`);
