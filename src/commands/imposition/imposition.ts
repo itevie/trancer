9d68e6364d0357d54fe520dd401a2c77a814205d
Isabella <izzyplays07@gmail.com>
2024-11-12 04:00:35 +0000

    no dawn ping for kona (x3)

--- src/commands/imposition/imposition.ts
+++ src/commands/imposition/imposition.ts
@@ -2,21 +2,21 @@ import { HypnoCommand } from "../../types/util";
 import { getRandomImposition } from "../../util/other";
 
 const command: HypnoCommand = {
-    name: "imposition",
-    aliases: ["i", "impo"],
-    description: "Send a some nice, fuzzy imposition",
-    type: "imposition",
-    usage: [
-        ["$cmd <user>", "Gives another user the imposition! :)"]
-    ],
+  name: "imposition",
+  aliases: ["i", "impo"],
+  description: "Send a some nice, fuzzy imposition",
+  type: "imposition",
+  usage: [["$cmd <user>", "Gives another user the imposition! :)"]],
 
-    handler: async (message, { oldArgs: args }) => {
-        if (args[0]) {
-            const user = args[0].replace(/[<>@]/g, "");
-            return message.channel.send(`${args.join(" ")} ${await getRandomImposition(user)}`);
-        }
-        return message.reply((await getRandomImposition(message.author.id)) + ` ${message.author.id === "735109350728663080" ? "<@395877903998648322>" : ""}`);
+  handler: async (message, { oldArgs: args }) => {
+    if (args[0]) {
+      const user = args[0].replace(/[<>@]/g, "");
+      return message.channel.send(
+        `${args.join(" ")} ${await getRandomImposition(user)}`
+      );
     }
-}
+    return message.reply(await getRandomImposition(message.author.id));
+  },
+};
 
 export default command;
