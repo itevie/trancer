11ca4cb7024bd62e2687ce99d8f9f6233417d326
Isabella <izzyplays07@gmail.com>
2024-11-25 00:18:33 +0000

    all together

---
+++ src/commands/leaderboards/allTogether.ts
@@ -0,0 +1,34 @@
+import { calculateLevel } from "../../messageHandlers/xp";
+import { HypnoCommand } from "../../types/util";
+import { getAllGuildsUserData } from "../../util/actions/userData";
+import { createEmbed } from "../../util/other";
+
+const command: HypnoCommand = {
+  name: "alltogetherleaderboard",
+  aliases: ["alltogetherlb", "alltogether", "allt", "alltlb"],
+  description: "Combines all the user data into one! (for the current server)",
+  type: "leaderboards",
+
+  handler: async (message) => {
+    const userData = await getAllGuildsUserData(message.guild.id);
+    const messages = userData.reduce((v, c) => v + c.messages_sent, 0);
+    const vcTime = userData.reduce((v, c) => v + c.vc_time, 0);
+    const xp = userData.reduce((v, c) => v + c.xp, 0);
+    const bumps = userData.reduce((v, c) => v + c.bumps, 0);
+
+    return message.reply({
+      embeds: [
+        createEmbed()
+          .setTitle("All our statistics together <3")
+          .setDescription(
+            `**Messages**: ${messages}:speech_balloon:\n` +
+              `**VC Time**: ${vcTime} minutes\n` +
+              `**XP**: ${xp} XP (Level ${calculateLevel(xp)})\n` +
+              `**Bumps**: ${bumps} bumps`
+          ),
+      ],
+    });
+  },
+};
+
+export default command;
