693ace6a0a607a5565fc62e2d500c67bdbc24cfe
Isabella <izzyplays07@gmail.com>
2024-12-08 15:18:33 +0000

    a

--- src/events/messageDelete.ts
+++ src/events/messageDelete.ts
@@ -2,22 +2,22 @@ import { client } from "..";
 import config from "../config";
 import { createEmbed } from "../util/other";
 
-client.on("messageDelete", async message => {
-    if (message.guild.id !== config.botServer.id) return;
+client.on("messageDelete", async (message) => {
+  if (message.guild.id !== config.botServer.id) return;
 
-    const channel = await client.channels.fetch(config.botServer.channels.logs);
-    if (channel.isTextBased()) {
-        await channel.send({
-            embeds: [
-                createEmbed()
-                    .setTitle(`Message deleted in #${(message.channel as any).name}`)
-                    .setDescription(message.content || "*No content*")
-                    .setColor("#FF0000")
-                    .setAuthor({
-                        iconURL: message.author.displayAvatarURL(),
-                        name: message.author.username
-                    })
-            ]
-        });
-    }
+  const channel = await client.channels.fetch(config.botServer.channels.logs);
+  if (channel.isTextBased()) {
+    await channel.send({
+      embeds: [
+        createEmbed()
+          .setTitle(`Message deleted in #${(message.channel as any).name}`)
+          .setDescription(message.content || "*No content*")
+          .setColor("#FF0000")
+          .setAuthor({
+            iconURL: message.author?.displayAvatarURL(),
+            name: message.author.username,
+          }),
+      ],
+    });
+  }
 });
