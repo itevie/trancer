3b573055d3f6d87ca7b0d2db1781c4eabc59803d
Isabella <izzyplays07@gmail.com>
2025-03-08 21:13:11 +0000

    Fix build script to ensure app directory is recreated and update subproject commit status

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 git pull
 tsc --build --verbose
 cp -r ./src/sql ./build/sql
-mkdir ./build/website/app
 rm -rf ./build/website/app
+mkdir ./build/website/app
 cp -r ./src/website/app/build ./build/website/app
