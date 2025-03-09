0ea22296f9c3702e08154224bf5e14d1804b486a
Isabella <izzyplays07@gmail.com>
2025-03-09 00:52:27 +0000

    Enhance leave message handling for guild members and update subproject commit status

--- src/events/leaves.ts
+++ src/events/leaves.ts
@@ -1,16 +1,41 @@
 import { client } from "..";
 import config from "../config";
 import { addToMemberCount } from "../util/analytics";
+import { actions } from "../util/database";
+import { replaceVarString } from "../util/language";
 import { autoBanned } from "./guildMemberAdd";
 
 client.on("guildMemberRemove", async (member) => {
   if (member.guild.id !== config.botServer.id) return;
+  let serverSettings = await actions.serverSettings.getFor(member.guild.id);
 
   // Add to analytics
   await addToMemberCount(member.guild.id, member.guild.memberCount);
 
   // Guards
-  if (member.guild.id !== config.botServer.id) return;
+
+  // Check for leave message in other servers
+  if (member.guild.id !== config.botServer.id) {
+    if (serverSettings.leave_channel_id) {
+      try {
+        const channel = await client.channels.fetch(
+          serverSettings.leave_channel_id
+        );
+        if (channel.isTextBased()) {
+          await channel.send(
+            replaceVarString(
+              serverSettings.leave_message,
+              member.user,
+              member.guild
+            )
+          );
+        }
+      } catch (e) {
+        console.log(`Failed to send leave message in ${member.guild.id}`, e);
+      }
+    }
+    return;
+  }
 
   const channel = await client.channels.fetch(
     config.botServer.channels.welcomes
