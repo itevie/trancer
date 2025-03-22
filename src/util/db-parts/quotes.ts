22d2dec1f7876e7e163ffb6c66b8965d1f828d32
Isabella <izzyplays07@gmail.com>
2025-03-22 22:26:18 +0000

    a

--- src/util/db-parts/quotes.ts
+++ src/util/db-parts/quotes.ts
@@ -1,10 +1,4 @@
-import {
-  Attachment,
-  EmbedBuilder,
-  Message,
-  TextBasedChannel,
-  User,
-} from "discord.js";
+import { EmbedBuilder, Message, TextBasedChannel, User } from "discord.js";
 import { database } from "../database";
 import { createEmbed } from "../other";
 import { client } from "../..";
@@ -166,107 +160,6 @@ const _actions = {
     } catch {
       fallback();
     }
-
-    return embed;
-  },
-
-  _generateEmbed: async (
-    quote: Quote,
-    hideUser: boolean = false,
-  ): Promise<EmbedBuilder> => {
-    // Fetch all deteails
-    const embed = createEmbed().setTitle("Quote");
-    let user: User | null = null;
-    let message: Message | null = null;
-    let image: string | null = null;
-    let messageLink = "";
-
-    // Try fetch user so it can show username
-    try {
-      user = await client.users.fetch(quote.author_id);
-    } catch {}
-
-    // Check if it is a newer quote so it can have replies
-    if (quote.message_id && quote.channel_id) {
-      try {
-        const channel = await client.channels.fetch(quote.channel_id);
-        if (channel.isTextBased()) {
-          message = await channel.messages.fetch(quote.message_id);
-        }
-      } catch {}
-    }
-
-    if (message) {
-      messageLink = `https://discord.com/channels/${message.guildId}/${message.channelId}/${message.id}`;
-
-      for (const embed of message.embeds) {
-        image =
-          embed.data.image?.proxy_url ||
-          embed.data.thumbnail?.proxy_url ||
-          image;
-      }
-
-      if (message.attachments.size > 0) {
-        const attachment = message.attachments.first();
-        if (
-          attachment &&
-          [
-            "image/png",
-            "image/jpeg",
-            "image/jpg",
-            "image/gif",
-            "image/gifv",
-          ].includes(attachment.contentType)
-        ) {
-          image = attachment.proxyURL;
-        }
-      }
-    }
-
-    if (image) embed.setImage(image);
-
-    let makeFooter = (user: User | string, message?: Message) => {
-      return `${
-        typeof user === "string" ? getUsernameSync(user) : user.username
-      } - ${message.createdAt.toDateString()}${
-        message
-          ? `- [[Message Link]](https://discord.com/channels/${message.guildId}/${message.channelId}/${message.id})`
-          : ""
-      }`;
-    };
-
-    let description = quote.content ? `*${quote.content}*` : "";
-    if (!hideUser) {
-      description += `\n - ${makeFooter(user ?? quote.author_id, message)}`;
-
-      if (message && message.embeds.length > 0) {
-        const msgEmbed = message.embeds[0];
-        embed.addFields({
-          name: `Embed: ${msgEmbed.title || "(No title)"}`,
-          value: msgEmbed.description || "(No description)",
-        });
-        if (!image && msgEmbed.image) embed.setImage(msgEmbed.image.url);
-        if (msgEmbed.thumbnail) embed.setThumbnail(msgEmbed.thumbnail.url);
-        if (msgEmbed.fields.length > 0) embed.addFields(...msgEmbed.fields);
-      }
-    }
-
-    if (message?.reference) {
-      try {
-        const ref = await message.fetchReference();
-        description += `\n:arrow_right_hook: *${ref.content}* - ${makeFooter(
-          ref.author,
-          ref,
-        )}`;
-      } catch {}
-    }
-
-    embed.setDescription(description.length === 0 ? "**" : description);
-    embed.setFooter({
-      text: hideUser
-        ? "Use guess (guess) to guess! (NO PREFIX)"
-        : `Quote #${quote.id}`,
-    });
 
     return embed;
   },
