636f5fb94c69a6846ed2e3ac1b3fc15edd5c827c
Isabella <izzyplays07@gmail.com>
2025-03-22 19:49:24 +0000

    Rewrite the embed generator

--- src/util/db-parts/quotes.ts
+++ src/util/db-parts/quotes.ts
@@ -1,7 +1,16 @@
-import { Attachment, EmbedBuilder, Message, User } from "discord.js";
+import {
+  Attachment,
+  EmbedBuilder,
+  Message,
+  TextBasedChannel,
+  User,
+} from "discord.js";
 import { database } from "../database";
 import { createEmbed } from "../other";
 import { client } from "../..";
+import { getUsernameSync } from "../cachedUsernames";
+import { addAbortSignal } from "stream";
+import { addMessageForCurrentTime } from "../analytics";
 
 const _actions = {
   add: async (message: Message, createdBy: string): Promise<Quote> => {
@@ -12,31 +21,31 @@ const _actions = {
       message.guild.id,
       message.id,
       message.channel.id,
-      createdBy
+      createdBy,
     );
     return (await database.get(
-      `SELECT * FROM quotes ORDER BY id DESC LIMIT 1`
+      `SELECT * FROM quotes ORDER BY id DESC LIMIT 1`,
     )) as Quote;
   },
 
   getById: async (id: number): Promise<Quote> => {
     return (await database.get(
       `SELECT * FROM quotes WHERE id = (?)`,
-      id
+      id,
     )) as Quote;
   },
 
   getForServer: async (serverId: string): Promise<Quote[]> => {
     return await database.all<Quote[]>(
       "SELECT * FROM quotes WHERE server_id = ?;",
-      serverId
+      serverId,
     );
   },
 
   getForUser: async (authorId: string): Promise<Quote[]> => {
     return await database.all<Quote[]>(
       "SELECT * FROM quotes WHERE author_id = ?;",
-      authorId
+      authorId,
     );
   },
 
@@ -46,10 +55,10 @@ const _actions = {
         if (from)
           return await database.get(
             `SELECT * FROM quotes WHERE server_id = (?) ORDER BY RANDOM() LIMIT 1;`,
-            from
+            from,
           );
         return await database.get(
-          `SELECT * FROM quotes ORDER BY RANDOM() LIMIT 1;`
+          `SELECT * FROM quotes ORDER BY RANDOM() LIMIT 1;`,
         );
       } catch {}
     }
@@ -63,7 +72,107 @@ const _actions = {
 
   generateEmbed: async (
     quote: Quote,
-    hideUser: boolean = false
+    isGame: boolean = false,
+  ): Promise<EmbedBuilder> => {
+    const embed = createEmbed().setTitle("Quote");
+    embed.setFooter({
+      text: isGame
+        ? "Use guess (guess) to guess! (NO PREFIX)"
+        : `Quote #${quote.id}`,
+    });
+
+    let makeFooter = (
+      user: User | string,
+      message?: Message,
+      small: boolean = false,
+    ) => {
+      return `${
+        typeof user === "string" ? getUsernameSync(user) : user.username
+      }${!small ? ` - ${message.createdAt.toDateString()}` : ""}${
+        message
+          ? ` - [[Message Link]](https://discord.com/channels/${message.guildId}/${message.channelId}/${message.id})`
+          : ""
+      }`;
+    };
+
+    let fallback = () => {
+      embed.setDescription(
+        `${quote.content.length !== 0 ? `*${quote.content}` : ""}\n - ${makeFooter(quote.author_id)}`,
+      );
+    };
+
+    if (!quote.message_id || !quote.channel_id) {
+      fallback();
+      return embed;
+    }
+
+    try {
+      const channel = (await client.channels.fetch(
+        quote.channel_id,
+      )) as TextBasedChannel;
+      const message = await channel.messages.fetch(quote.message_id);
+
+      // Main quote
+      let description =
+        message.content.length > 0 ? `*${message.content}*` : "";
+      if (!isGame) description += "\n - " + makeFooter(message.author, message);
+
+      // Check for reference
+      if (message.reference) {
+        let reference = await message.fetchReference();
+        let amount = 1;
+
+        do {
+          description += `\n:arrow_right_hook: ${
+            reference.content.length === 0 ? "" : `*${reference.content}*`
+          }`;
+          if (!isGame)
+            description += ` - ${makeFooter(reference.author, reference, true)}`;
+          reference = reference.reference
+            ? await reference.fetchReference()
+            : null;
+        } while (reference !== null && amount < 5);
+      }
+
+      embed.setDescription(description);
+
+      // Check if it has an embed
+      if (message.embeds.length > 0) {
+        const msgEmbed = message.embeds[0];
+        embed.addFields({
+          name: `Embed: ${msgEmbed.title || "(No title)"}`,
+          value: msgEmbed.description || "(No description)",
+        });
+        if (msgEmbed.thumbnail) embed.setThumbnail(msgEmbed.thumbnail.url);
+        if (msgEmbed.image) embed.setImage(msgEmbed.image.url);
+        if (msgEmbed.fields.length > 0) embed.addFields(...msgEmbed.fields);
+      }
+
+      if (message.attachments.size > 0) {
+        const attachment = message.attachments.first();
+        if (
+          attachment &&
+          [
+            "image/png",
+            "image/jpeg",
+            "image/jpg",
+            "image/gif",
+            "image/gifv",
+          ].includes(attachment.contentType)
+        ) {
+          embed.setImage(attachment.proxyURL);
+        }
+      }
+    } catch {
+      fallback();
+    }
+
+    return embed;
+  },
+
+  _generateEmbed: async (
+    quote: Quote,
+    hideUser: boolean = false,
   ): Promise<EmbedBuilder> => {
     // Fetch all deteails
     const embed = createEmbed().setTitle("Quote");
@@ -116,12 +225,19 @@ const _actions = {
 
     if (image) embed.setImage(image);
 
+    let makeFooter = (user: User | string, message?: Message) => {
+      return `${
+        typeof user === "string" ? getUsernameSync(user) : user.username
+      } - ${message.createdAt.toDateString()}${
+        message
+          ? `- [[Message Link]](https://discord.com/channels/${message.guildId}/${message.channelId}/${message.id})`
+          : ""
+      }`;
+    };
+
     let description = quote.content ? `*${quote.content}*` : "";
     if (!hideUser) {
-      description += `\n - ${
-        user ? user.username : "*(failed to get user)*"
-      } - ${new Date(quote.created_at).toDateString()}`;
-      if (messageLink) description += ` - [Message Link](${messageLink})`;
+      description += `\n - ${makeFooter(user ?? quote.author_id, message)}`;
 
       if (message && message.embeds.length > 0) {
         const msgEmbed = message.embeds[0];
@@ -138,7 +254,10 @@ const _actions = {
     if (message?.reference) {
       try {
         const ref = await message.fetchReference();
-        description += `\n:arrow_right_hook: ${ref.content}`;
+        description += `\n:arrow_right_hook: *${ref.content}* - ${makeFooter(
+          ref.author,
+          ref,
+        )}`;
       } catch {}
     }
 
