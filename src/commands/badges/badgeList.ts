0b857fbb22d6cf235c358f82829134d7ceb94be7
Isabella <izzyplays07@gmail.com>
2025-06-23 12:27:00 +0100

    turn badgelist into paginated response

--- src/commands/badges/badgeList.ts
+++ src/commands/badges/badgeList.ts
@@ -3,6 +3,7 @@ import { HypnoCommand } from "../../types/util";
 import badges from "../../util/badges";
 import { createEmbed } from "../../util/other";
 import { actions } from "../../util/database";
+import { paginate } from "../../util/components/pagination";
 
 const command: HypnoCommand = {
   name: "badgelist",
@@ -12,24 +13,29 @@ const command: HypnoCommand = {
 
   handler: async (message) => {
     let aquiredBadges = await actions.badges.aquired.getAll();
-    let text = "";
+    let text: string[] = [];
 
     for (let i in badges) {
       let amount = aquiredBadges.filter((x) => x.badge_name === i).length;
       let theUser: User | null;
       if (amount === 1)
         theUser = await message.client.users.fetch(
-          aquiredBadges.find((x) => x.badge_name === i).user
+          aquiredBadges.find((x) => x.badge_name === i).user,
         );
-      text += `${badges[i].emoji} \`${badges[i].name}\` *${
-        badges[i].description
-      }*\n - ${amount} ${amount === 1 ? "person has" : "people have"} this${
-        theUser ? ` (${theUser.username})` : ""
-      }\n\n`;
+      text.push(
+        `${badges[i].emoji} \`${badges[i].name}\` *${
+          badges[i].description
+        }*\n - ${amount} ${amount === 1 ? "person has" : "people have"} this${
+          theUser ? ` (${theUser.username})` : ""
+        }\n`,
+      );
     }
 
-    return message.reply({
-      embeds: [createEmbed().setTitle(`List of badges`).setDescription(text)],
+    return paginate({
+      message,
+      embed: createEmbed().setTitle("List of badges"),
+      type: "description",
+      data: text,
     });
   },
 };
